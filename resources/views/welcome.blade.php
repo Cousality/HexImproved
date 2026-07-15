32f987135d63d1f63861adc6a31e4491cc030cbe
Adriana <svecaadriana05@gmail.com>
2026-07-15 19:39:40 +0100

    changed the title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Cole Was here</title>
+        <title>Cole Was here, fuck this shii </title> 
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
