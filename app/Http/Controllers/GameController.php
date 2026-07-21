b31f9ac3bc1fb90eb1d03fca4f7debe274a26f1e
Cousality <165505726+Cousality@users.noreply.github.com>
2026-07-21 13:25:17 +0100

    Created GameController

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class GameController extends Controller
+{
+    public $BoardSize = 9;
+
+    public $Board = [];
+
+    public function show()
+    {
+    }
+}
