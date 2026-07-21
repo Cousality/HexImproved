366d7bc8501064004997bf2db73d18a1df49fd4a
Cousality <165505726+Cousality@users.noreply.github.com>
2026-07-21 14:41:12 +0100

    red and blue alternate

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -2,15 +2,14 @@
 
     @include('components.nav')
 
-    <svg width="900" height="700">
+    <svg width="1500" height="1500">
 
         @for ($row = 0; $row < 5; $row++)
 
             @for ($column = 0; $column < 5; $column++)
-
                 @php
-                    $x = 100 + ($column * 96) + ($row * 48);
-                    $y = 100 + ($row * 92);
+                    $x = 100 + $column * 96 + $row * 48;
+                    $y = 100 + $row * 92;
                 @endphp
 
                 <polygon
@@ -22,22 +21,25 @@
                         {{ $x - 48 }},{{ $y + 32 }}
                         {{ $x - 48 }},{{ $y - 32 }}
                     "
-                    fill="lightgray"
-                    stroke="black"
-                    data-row="{{ $row }}"
-                    data-column="{{ $column }}"
-                    onClick="hexClicked(this)"
-                />
-
+                    fill="lightgray" stroke="black" data-row="{{ $row }}" data-column="{{ $column }}"
+                    onClick="hexClicked(this)" />
             @endfor
 
         @endfor
 
     </svg>
-    <script> 
-    
+    <script>
+        let player1 = false;
+
         function hexClicked(hex) {
-            hex.setAttribute('fill', 'lightblue');
+            if (player1 == false) {
+                hex.setAttribute('fill', 'blue');
+                player1 = true;
+            } else {
+                hex.setAttribute('fill', 'red');
+                player1 = false;
+            }
+
         }
     </script>
 
